72b4d418b291a62f41fd609a12bbd4681f3cf426
Ashkan Mohseni <amohseni80@yahoo.com>
2020-10-16 23:20:49 -0400

    Path Variables(5.2)

--- src/main/java/ca/sheridancollege/mohsenas/controller/StudentController.java
+++ src/main/java/ca/sheridancollege/mohsenas/controller/StudentController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import ca.sheridancollege.mohsenas.beans.Student;
 import ca.sheridancollege.mohsenas.database.DatabaseAccess;
@@ -17,18 +20,68 @@ public class StudentController {
 	@GetMapping("/")
 	public String index(Model model) {
 		
+		// Add student Frank to the DB
 		da.insertStudent();
-		model.addAttribute("Student", new Student());
+		
+		model.addAttribute("student", new Student());
 		
 		return "index";
 	}
 	
 	
 	
-//	@PostMapping("/insertStudent")
-//	public String insertStudent() {
-//		
-//		return "index";
-//	}
+	@PostMapping("/insertStudent")
+	public String insertStudent(Model model, @ModelAttribute Student student) {
+		
+		//Add the specified name by the user to the DB
+		da.insertStudent(student);
+		
+		// Get the student list and add it to the model (for index.html to use it)
+		model.addAttribute("studentList", da.getList());
+		
+		// Add a blank object to insert the next student
+		model.addAttribute("student", new Student());
+		
+		return "index";
+	}
+	
+	
+	@GetMapping("/deleteStudentById/{id}")
+	public String deleteStudentById(Model model, @PathVariable Long id) {
+		
+		// Call method to delete the record
+		da.deleteStudentbyID(id);
+		
+		// Update the list
+		model.addAttribute("student", da.getList());
+		// Blank object for form binding 
+		model.addAttribute("student", new Student());
+		
+		
+		return "index";
+	}
+	
+	@GetMapping("/editStudentById/{id}")
+	public String editStudentById(Model model, @PathVariable Long id) {
+		
+		//Specify a record to update and get the result
+		Student student = da.getStudentListByID(id).get(0);
+		
+		// Add the object to the model
+		model.addAttribute("student", student);
+		
+		// Delete the existing version of the record
+		da.deleteStudentbyID(id);
+		
+		// Update the model with the new version of the list
+		model.addAttribute("studentList", da.getList());
+		
+		
+		
+		return "index";
+		
+	}
+	
+	
 	
 }
--- src/main/java/ca/sheridancollege/mohsenas/database/DatabaseAccess.java
+++ src/main/java/ca/sheridancollege/mohsenas/database/DatabaseAccess.java
@@ -1,34 +1,121 @@
 package ca.sheridancollege.mohsenas.database;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import ca.sheridancollege.mohsenas.beans.Student;
 
 @Repository
 public class DatabaseAccess {
 
 	@Autowired
 	protected NamedParameterJdbcTemplate jdbc;
-	
+
+	/*
+	 * Insert Frank to the database
+	 */
+
 	public void insertStudent() {
-		
+
 		// To use for parameters
 		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+
 		// Specify the query
 		String query="INSERT INTO student(name) VALUES ('Frank')";
-		
+
 		// Insert the row to the DB and check the return value
 		int rowsAffected = jdbc.update(query, namedParameters);
-		
+
 		// Check if any rows were updated
 		if(rowsAffected>0) {
 			System.out.println("Student was inserted in DB");
 		}
+
+
+	}
+
+	/*
+	 * Insert a student object to the database
+	 */
+
+	public void insertStudent(Student student) {
+
+		// To use for parameters
+		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+
+		// Specify the query to insert the student name to the DB
+		String query="INSERT INTO student(name) VALUES (:name)";
 		
+		//Specify the value to replace name
+		namedParameters.addValue("name", student.getName());
+
+		// Insert Student to DB
+		int rowsAffected = jdbc.update(query, namedParameters);
+
+		// Check to see if the insert was successful
+		if(rowsAffected>0) {
+			System.out.println("Student was inserted in DB");
+		}
+
+	}
+	
+	/*
+	 * Display a List of all students
+	 * @return the Student List
+	 */
+	public List<Student> getList(){
+		
+		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+		
+		//Query to retrieve all students
+		String query = "SELECT * FROM student";
+		
+		// Return the list
+		// NOTE: BeanPropertyRowMapper will convert the data read from the DB to student object
+		return jdbc.query(query, namedParameters,new BeanPropertyRowMapper<Student>(Student.class));
+		
+	}
+	
+	
+	public void deleteStudentbyID(Long ID) {
+		
+		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+		
+		//Query
+		String query = "DELETE FROM student WHERE id = :id";
+		
+		//specify the parameter
+		namedParameters.addValue("id", ID);
+		
+		// Perform delete
+		int rowsAffected = jdbc.update(query, namedParameters);
+		
+		//Check if delete is successful
+		if(rowsAffected > 0) {
+			System.out.println("Student with id (" + ID + ") has been deleted.");
+		}
 		
 	}
 	
 	
 	
-}
+	public List<Student> getStudentListByID(Long ID){
+		
+		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+		
+		String query= "SELECT * FROM student WHERE id = :id";
+		
+		// Specify the value of the named parameter
+		namedParameters.addValue("id", ID);
+		
+		return jdbc.query(query, namedParameters, new BeanPropertyRowMapper<Student>(Student.class));
+		
+	}
+
+
+}// End of DatabaseAccess
